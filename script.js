f71a56b95bf3d08f2685952e3b19eb01e0b3cda3
tnahddisttud <37979764+tnahddisttud@users.noreply.github.com>
2020-09-24 23:22:08 +0530

    Fixed Multiplication Bug

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ class Calculator {
       case '-':
         computation = prev - current
         break
-      case '*':
+      case '×':
         computation = prev * current
         break
       case '÷':
